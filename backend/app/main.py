73b43e3d83fd006b0be53326ef97c7361ce37241
RohanBade <baderohan111@gmail.com>
2024-11-25 15:41:07 +0545

    add basic main structure for file

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,14 +1,20 @@
-from fastapi import FastAPI, UploadFile
+from fastapi import Depends, FastAPI, UploadFile
 
+from backend.app import schemas
+from backend.app.core.oauth2 import get_current_user
 from backend.app.database import database
 from backend.app.database.database import engine
+from backend.app.routers.authentication import router as auth_router
 from backend.app.routers.user import router as user_router
 
 app = FastAPI()
 database.Base.metadata.create_all(engine)
 app.include_router(user_router)
+app.include_router(auth_router)
 
 
-@app.post("/uploadfile/")
-async def create_upload_file(file: UploadFile):
+@app.post("/upload/", tags=["uploads"])
+async def create_upload_file(
+    file: UploadFile, current_user: schemas = Depends(get_current_user)
+):
     return {"filename": file.filename}
