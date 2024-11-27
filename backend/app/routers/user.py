05862ed6be19a80af5cf222f45460fa8d0bc48e9
RohanBade <baderohan111@gmail.com>
2024-11-27 16:00:11 +0545

    add router for all users

--- backend/app/routers/user.py
+++ backend/app/routers/user.py
@@ -1,7 +1,9 @@
+from typing import List
+
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 
-from backend.app.crud.user import create, read
+from backend.app.crud.user import create, read, read_all
 from backend.app.database.database import get_db
 from backend.app.schemas.user import ShowUser, User
 
@@ -16,3 +18,8 @@ def create_user(request: User, db: Session = Depends(get_db)):
 @router.get("/get/{id}", response_model=ShowUser)
 def get_user(id: int, db: Session = Depends(get_db)):
     return read(id, db)
+
+
+@router.get("/get", response_model=List[ShowUser])
+def get_user(db: Session = Depends(get_db)):
+    return read_all(db)
