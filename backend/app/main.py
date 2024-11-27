418c199eb0e8192f8f027d5b952e010ee62f05c4
RohanBade <baderohan111@gmail.com>
2024-11-27 15:59:27 +0545

    add all cors

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,4 +1,5 @@
 from fastapi import Depends, FastAPI, UploadFile
+from fastapi.middleware.cors import CORSMiddleware
 
 from backend.app import schemas
 from backend.app.core.oauth2 import get_current_user
@@ -8,6 +9,16 @@ from backend.app.routers.authentication import router as auth_router
 from backend.app.routers.user import router as user_router
 
 app = FastAPI()
+
+# for now allowing all CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 database.Base.metadata.create_all(engine)
 app.include_router(user_router)
 app.include_router(auth_router)
