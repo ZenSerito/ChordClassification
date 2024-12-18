17d31bd341d27c66ed9c0e7597e0bb1ec00389e0
RohanBade <baderohan111@gmail.com>
2024-12-18 19:09:21 +0545

    feat: add functionality to classify chords using svm model

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,10 +1,16 @@
+import os
+import pickle
+
+import pandas as pd
 from fastapi import Depends, FastAPI, UploadFile
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import JSONResponse
 
 from backend.app import schemas
 from backend.app.core.oauth2 import get_current_user
 from backend.app.database import database
 from backend.app.database.database import engine
+from backend.app.preprocessing.svm_predict import PitchClassProfiler, consolidate_chords
 from backend.app.routers.authentication import router as auth_router
 from backend.app.routers.user import router as user_router
 
@@ -24,8 +30,53 @@ app.include_router(user_router)
 app.include_router(auth_router)
 
 
+svm_model_path = os.path.join(os.path.dirname(__file__), "saved_models", "model.pkl")
+
+
 @app.post("/upload/", tags=["uploads"])
 async def create_upload_file(
     file: UploadFile, current_user: schemas = Depends(get_current_user)
 ):
-    return {"filename": file.filename}
+    try:
+        pitch_classes = [
+            "C",
+            "C#",
+            "D",
+            "D#",
+            "E",
+            "F",
+            "F#",
+            "G",
+            "G#",
+            "A",
+            "A#",
+            "B",
+        ]
+        chords = ["A", "Am", "Bm", "C", "D", "Dm", "E", "Em", "F", "G"]
+
+        temp_file_path = f"temp_{file.filename}"
+        with open(temp_file_path, "wb") as temp_file:
+            temp_file.write(await file.read())
+
+        profiler = PitchClassProfiler(temp_file_path)
+        pcp_profiles, timestamps = profiler.get_profiles(
+            window_duration=1.0, overlap=0.0
+        )
+
+        with open(svm_model_path, "rb") as f:
+            model = pickle.load(f)
+        results = []
+        for i, profile in enumerate(pcp_profiles):
+            profile_df = pd.DataFrame([profile], columns=pitch_classes)
+            prediction = model.predict(profile_df)
+            index = int(prediction[0])
+            predicted_chord = chords[index]
+            results.append({"timestamp": timestamps[i], "chord": predicted_chord})
+
+        consolidated_results = consolidate_chords(results)
+
+        os.remove(temp_file_path)
+
+        return {"filename": file.filename, "predictions": consolidated_results}
+    except Exception as e:
+        return JSONResponse(status_code=500, content={"error": str(e)})
